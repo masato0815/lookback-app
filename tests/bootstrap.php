ed11eefdd71183bf329b1481fbce4ded9d78513c
金野雅人 <konnomasato@konnomasatonoMacBook-Air.local>
2021-11-01 21:31:07 +0900

    first commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,3 +10,8 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 $_SERVER['PHP_SELF'] = '/';
+
+
+if (isset($_ENV['CAKE_ENV'])) { // このif文を追加
+    Configure::load('app_' . $_ENV['CAKE_ENV'], 'default'); // このif文を追加
+}
